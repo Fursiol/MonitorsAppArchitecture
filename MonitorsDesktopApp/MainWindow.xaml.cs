f096727d38a37c842b3bde30fdf791571000f99c
Jakub <fursikjf@gmail.com>
2024-01-25 05:51:00 +0100

    DesktopApp Started

---
+++ MonitorsDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.Configuration;
+using MonitorsApp.BLC;
+
+namespace MonitorsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ProducersViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+            var libraryPath = configuration.GetSection("DAOLibrary:Path").Value;
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            BLC.BLC blc = new BLC.BLC(libraryPath, connectionString);
+
+            _viewModel = new ProducersViewModel(blc);
+            DataContext = _viewModel;
+        }
+    }
+}
---
+++ MonitorsDesktopApp/ProducersViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MonitorsApp.BLC;
+using MonitorsApp.Interfaces;
+using System.Windows.Input;
+using System.Windows;
+
+namespace MonitorsApp
+{
+    public class ProducersViewModel : INotifyPropertyChanged
+    {
+        private BLC.BLC _blc;
+
+        private ObservableCollection<IProducer> _producers;
+        public ObservableCollection<IProducer> Producers
+        {
+            get => _producers;
+            set
+            {
+                _producers = value;
+                OnPropertyChanged(nameof(Producers));
+            }
+        }
+
+        public ProducersViewModel(BLC.BLC blc)
+        {
+            _blc = blc;
+            Producers = new ObservableCollection<IProducer>(blc.GetProducers());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _filter;
+
+        public string Filter
+        {
+            get => _filter;
+            set
+            {
+                if (_filter != value)
+                {
+                    _filter = value;
+                    OnPropertyChanged();
+                    FilterProducers();
+                }
+            }
+        }
+        private void FilterProducers()
+        {
+            if (string.IsNullOrEmpty(_filter))
+            {
+                Producers = new ObservableCollection<IProducer>(_blc.GetProducers());
+            }
+            else
+            {
+                var filteredProducers = _blc.GetProducers()
+                    .Where(p => p.Name.Contains(Filter, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+                Producers = new ObservableCollection<IProducer>(filteredProducers);
+            }
+        }
+    }
+}
